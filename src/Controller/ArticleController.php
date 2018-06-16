35809839059d9f593ce79ab5c1636159fbbe00c1
Elise Griset <elise.griset@gmail.com>
2018-06-16 16:30:57 +0200

    like + home page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,14 +10,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ArticleController extends AbstractController
 {
   /**
-   * @Route("/")
+   * @Route("/", name="app_homepage")
    */
   public function homepage()
   {
-return new Response('OMG MY FIRST PAGE');  }
+// return new Response('OMG MY FIRST PAGE'); 
+return $this->render('article/homepage.html.twig');
+}
 
 /**
- * @Route("/news/{slug}")
+ * @Route("/news/{slug}", name="article_show")
  */
 public function show($slug){
 $comments = [
